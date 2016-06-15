f53b0f0a6419c1432c12e4224841b6254e201aab
Alex Lavoie <alavoie@devicebits.com>
2016-06-15 16:13:17 -0400

    add api constraints and setup test framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json },
                                                 constraints: { subdomain: 'api' }, path: '/' do
-    # we are going to list our resources here
+    scope module: :v1,
+                    constraints: ApiConstraints.new(version: 1, default: true) do
+      # we are going to list our resources here
+    end
   end
 end
