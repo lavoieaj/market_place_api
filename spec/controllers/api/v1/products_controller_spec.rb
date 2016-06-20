5ca3a399a4b14b2c0e3f3af69b48e6fee1f310a9
Alex Lavoie <alavoie@devicebits.com>
2016-06-20 09:06:11 -0400

    embed the user into product json output

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       expect(product_response[:title]).to eql @product.title
     end
 
+    it "has the user as a embeded object" do
+      product_response = json_response
+      expect(product_response[:user][:email]).to eql @product.user.email
+    end
+
     it { should respond_with 200 }
   end
 
@@ -27,6 +32,13 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     it "returns 4 records from the database" do
       products_response = json_response
       expect(products_response.length).to eq(5)
+    end
+
+    it "returns the user object into each product" do
+      products_response = json_response
+      products_response.each do |product_response|
+        expect(product_response[:user]).to be_present
+      end
     end
 
     it { should respond_with 200 }
