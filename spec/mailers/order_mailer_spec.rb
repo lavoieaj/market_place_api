00e0e5ea655f69be397c979ff90700e15148d97f
Alex Lavoie <alavoie@devicebits.com>
2016-06-21 09:05:26 -0400

    refactor order mailer spec to resolve deprecation warnings

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -12,23 +12,28 @@ RSpec.describe OrderMailer, type: :mailer do
     end
 
     it "should be set to be delivered to the user from the order passed in" do
-      @order_mailer.should deliver_to(@user.email)
+      #@order_mailer.should deliver_to(@user.email)
+      expect(@order_mailer).to deliver_to(@user.email)
     end
 
     it "should be set to be sent from no-reply@marketplace.com" do
-      @order_mailer.should deliver_from('no-reply@marketplace.com')
+      #@order_mailer.should deliver_from('no-reply@marketplace.com')
+      expect(@order_mailer).to deliver_from('no-reply@marketplace.com')
     end
 
     it "should contain the user's message in the mail body" do
-      @order_mailer.should have_body_text(/Order: ##{@order.id}/)
+      #@order_mailer.should have_body_text(/Order: ##{@order.id}/)
+      expect(@order_mailer).to have_body_text(/Order: ##{@order.id}/)
     end
 
     it "should have the correct subject" do
-      @order_mailer.should have_subject(/Order Confirmation/)
+      #@order_mailer.should have_subject(/Order Confirmation/)
+      expect(@order_mailer).to have_subject(/Order Confirmation/)
     end
 
     it "should have the products count" do
-      @order_mailer.should have_body_text(/You ordered #{@order.products.count} products:/)
+      #@order_mailer.should have_body_text(/You ordered #{@order.products.count} products:/)
+      expect(@order_mailer).to have_body_text(/You ordered #{@order.products.count} products:/)
     end
 
   end
